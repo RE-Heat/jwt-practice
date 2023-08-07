6cdcd51c747964fcc129cf881008d6d3017404bf
reheat <reheat1540@gmail.com>
2023-08-07 23:20:46 +0900

    feat : password Encoding해서 저장하기 - 스프링 시큐리티 적용 [#1]

--- src/main/java/jwtpractice/jwtpractice/service/UserService.java
+++ src/main/java/jwtpractice/jwtpractice/service/UserService.java
@@ -5,13 +5,14 @@ import jwtpractice.jwtpractice.exception.AppException;
 import jwtpractice.jwtpractice.exception.ErrorCode;
 import jwtpractice.jwtpractice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
-
+    private final BCryptPasswordEncoder encoder;
     public String join(String userName, String password) {
         //username 중복 check
         userRepository.findByUserName(userName)
@@ -22,7 +23,7 @@ public class UserService {
         //저장
         User user = User.builder()
                 .userName(userName)
-                .password(password)
+                .password(encoder.encode(password))
                 .build();
         userRepository.save(user);
 
