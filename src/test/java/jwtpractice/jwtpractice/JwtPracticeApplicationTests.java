e7113a0a7fa97704d10f30346a738ef3018a960d
reheat <reheat1540@gmail.com>
2023-08-07 10:55:11 +0900

    initial commit

---
+++ src/test/java/jwtpractice/jwtpractice/JwtPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package jwtpractice.jwtpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
